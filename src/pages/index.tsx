69899eeba9e6a6bb6cd459e848d02415deaf7f7e
Samuel Nicholls <samuel.nicholls@hey.com>
2023-08-25 09:54:31 +0100

    Getting SWR working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next';
 import usePosts from './api/usePosts';
+import { Post } from '@/types';
 
 const Home: NextPage = () => {
   const { data: posts, isLoading } = usePosts(1);
-  console.log("usePosts.posts", posts)
+  // eslint-disable-next-line no-console
   console.log("usePosts.isLoading", isLoading)
 
   return (
     <div className="container mx-auto">
       <h1>NextJS Starter</h1>
+      {posts?.map((post: Post, index: number) => (
+        <div key={index}>
+          <p>{post.userId}</p>
+        </div>
+      ))}
     </div>
   );
 };
